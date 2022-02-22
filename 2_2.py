112da3b1c54b2c0d0c96ba656cf63fa186909342
endtry <lucas123phan@gmail.com>
2022-02-22 15:32:44 -0800

    adding chapter 2-2

---
+++ 2_2.py
@@ -0,0 +1,24 @@
+
+# myvar = "Say it ain't so"
+# print(myvar)
+
+
+# var2 = "Spam & Eggs"
+
+# spam = "      Spam"
+# eggs = "Eggs      "
+# spam_and_eggs = spam + " & " + eggs
+
+# print(spam_and_eggs[:])
+
+# length = len(spam_and_eggs)
+# print(length)
+# print(spam_and_eggs.upper())
+
+
+# praise = "Good Doggie"
+
+# count = praise.upper().count("G")
+# print("Count: ", count)
+
+full_name = input("enter your full name: ")
