e1f733660e7a4b500e97274687437cdbaeee49f8
endtry <lucas123phan@gmail.com>
2022-02-22 14:16:49 -0800

    initial commit

---
+++ 2_1.py
@@ -0,0 +1,52 @@
+
+
+from dis import dis
+from time import time
+from tkinter import E
+from traceback import print_tb
+
+
+# print("3/2=",3/2)
+
+# speed = 50
+# time_elapsed = 14
+# distance = speed * time_elapsed
+# print(distance)
+
+"""
+this is a multi line comment
+"""
+# This is a one line comment
+
+
+# a = 2
+# b = 3
+# print(abs(1-4*b))
+
+
+# num = 4
+
+
+# num2 = num
+
+# num3 = 0
+# num3 += num2
+# num3 -= 2
+# print("num3", num3)
+# x = 0
+
+# x = x - 1 # x -=1
+
+
+
+
+# inches = 10 // 3
+
+# print("inches", inches)
+
+
+# print("this is a valid line of code")
+
+n = 5
+n = 7
+
