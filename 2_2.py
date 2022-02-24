5b615f503ac7ffe3c370f2d980786304b88783f7
endtry <lucas123phan@gmail.com>
2022-02-24 14:47:44 -0800

    adding chapter 2 code

--- 2_2.py
+++ 2_2.py
@@ -21,4 +21,32 @@
 # count = praise.upper().count("G")
 # print("Count: ", count)
 
-full_name = input("enter your full name: ")
+# Lucas Phan
+# full_name = input("Enter your full name: ")
+# index_of_space = full_name.rfind(" ")
+# first_name = full_name[:index_of_space]
+# last_name = full_name[index_of_space+1:]
+# print("First_name", first_name)
+# print("Last name", last_name)
+
+# annual_income = eval(input("Enter your annual income: "))
+# print("User's income is ", annual_income)
+# monthly_income = annual_income/12
+# print("monthly income", monthly_income)
+
+# var1 = 1
+# var2 = 2
+# var3 = "3"
+# var4 = "4"
+
+# var5 = var1+var2 # 3  Int
+# var6 = var3+var4 # 34 # String
+
+str1 = "Python"
+length = len(str1) #5 Int
+str2 = str1[:] #Python String
+print("str2", str2)
+str3 = str1[:10] #Python
+print("str3", str3)
+str4 = str2[2:-1] #Tho
+print("str4", str4)
