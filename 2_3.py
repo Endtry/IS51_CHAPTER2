e221c4c9960589eae8995fe3abc130b2af5f08f2
endtry <lucas123phan@gmail.com>
2022-02-24 15:28:01 -0800

    adding new code

--- 2_3.py
+++ 2_3.py
@@ -1,1 +1,12 @@
-print("chapter 2-3    ")
+print("one", "two", "three", sep=", ")
+
+print("Hello", end=" ")
+print("World!")
+
+
+print("a\tb\tc")
+print("a\tb\tc\nd\te\tf")
+
+
+print("\\Hello World!\\") # "Hello World!"
+
